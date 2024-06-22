3f8524fd72c67fa86beac63372db72a311a62539
fiorde90 <fiorde_90@hotmail.com>
2024-06-22 15:09:32 -0300

    change package "functional" to "functional_teacher_theory"

---
+++ modules/src/main/java/com/platzi/functional_teacher_theory/_04_functional/CLIArguments.java
@@ -0,0 +1,9 @@
+package com.platzi.functional_teacher_theory._04_functional;
+
+public class CLIArguments {
+    private boolean isHelp;
+
+    public boolean isHelp() {
+        return isHelp;
+    }
+}
---
+++ modules/src/main/java/com/platzi/functional_teacher_theory/util/Utils.java
@@ -0,0 +1,10 @@
+package com.platzi.functional_teacher_theory.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Utils {
+    public static <T> List<T> getListOf(T... items) {
+        return Arrays.asList(items);
+    }
+}
