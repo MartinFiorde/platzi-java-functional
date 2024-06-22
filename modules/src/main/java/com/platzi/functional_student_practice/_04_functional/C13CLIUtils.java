d1dba30c51627c26e3369aef6049a539abca8e4a
fiorde90 <fiorde_90@hotmail.com>
2024-06-22 17:50:14 -0300

    implemented supplier and consumer

--- modules/src/main/java/com/platzi/functional_student_practice/_04_functional/C13CLIUtils.java
+++ modules/src/main/java/com/platzi/functional_student_practice/_04_functional/C13CLIUtils.java
@@ -1,5 +1,7 @@
 package com.platzi.functional_student_practice._04_functional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -11,17 +13,23 @@ public class C13CLIUtils {
         System.out.println("CLASS 13\n");
 
         // 1. Created CLI class, consumer and supplier
+        List<CLIArguments> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(generateCLI());
+            list.get(i).setHelp(i % 2 == 0);
+        }
 
-        System.out.println("////////////////////////////////////////////////////////////////////////////////////\n");
-        System.out.println("CLASS 14\n");
+        for (int i = 0; i < list.size(); i++) {
+            showHelp(list.get(i), i);
+        }
 
     }
 
 
-    private static void showHelp(CLIArguments cliArguments) {
+    private static void showHelp(CLIArguments cliArguments, int i) {
         Consumer<CLIArguments> consumerHelper = cliArguments1 -> {
             if (cliArguments1.isHelp()) {
-                System.out.println("Manual required");
+                System.out.println(i +": Manual required");
             }
         };
         consumerHelper.accept(cliArguments);
@@ -29,7 +37,7 @@ public class C13CLIUtils {
 
 
     private static CLIArguments generateCLI() {
-        Supplier<CLIArguments> generator = () -> new CLIArguments();
+        Supplier<CLIArguments> generator = CLIArguments::new; //same as () -> new CLIArguments()
         return generator.get();
     }
 
@@ -37,15 +45,12 @@ public class C13CLIUtils {
     private static class CLIArguments {
         private boolean isHelp;
 
-        public CLIArguments() {
-        }
-
-        public CLIArguments(boolean isHelp) {
-            this.isHelp = isHelp;
-        }
-
         public boolean isHelp() {
             return isHelp;
+        }
+
+        public void setHelp(boolean help) {
+            isHelp = help;
         }
     }
 
