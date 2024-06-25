cae537b029dc6ce2b5f2ca0403a897ddc3b151c1
fiorde90 <fiorde_90@hotmail.com>
2024-06-25 10:00:54 -0300

    class 8 inmutable and update to java17 for tests with List.of()

---
+++ modules/src/main/java/com/platzi/functional_student_practice/_03_inmutable/C8InmutObjWithMutAttributes.java
@@ -0,0 +1,71 @@
+package com.platzi.functional_student_practice._03_inmutable;
+
+import java.util.List;
+
+public class C8InmutObjWithMutAttributes {
+
+    public static void main(String[] args) {
+
+        System.out.println("////////////////////////////////////////////////////////////////////////////////////");
+        System.out.println("\nCLASS 8\n");
+
+        // 1. We make an inmutable object (without setters)
+        Direccion dir = new Direccion("New York");
+
+        // 2. We make a mutable object (with setters)
+        Persona p1 = new Persona(dir);
+
+        // 3. We make an inmutable list
+        List<Persona> lista1 = List.of(p1);
+        List<Persona> lista2 = List.copyOf(lista1);
+
+
+
+        System.out.println("Antes de modificar: " + lista1);
+
+        // Modificando un elemento mutable contenido en la lista1 inmutable
+        p1.getDireccion().setCiudad("Los Angeles");
+
+        System.out.println("Después de modificar: " + lista1);
+
+    }
+
+    private static class Direccion {
+        String ciudad;
+
+        Direccion(String ciudad) {
+            this.ciudad = ciudad;
+        }
+
+        String getCiudad() {
+            return ciudad;
+        }
+
+        void setCiudad(String ciudad) {
+            this.ciudad = ciudad;
+        }
+
+        @Override
+        public String toString() {
+            return ciudad;
+        }
+    }
+
+    private static class Persona {
+        private final Direccion direccion;
+
+        Persona(Direccion direccion) {
+            this.direccion = direccion;
+        }
+
+        public Direccion getDireccion() {
+            return direccion;
+        }
+
+        @Override
+        public String toString() {
+            return "Persona{direccion=" + direccion + "}";
+        }
+    }
+
+}
