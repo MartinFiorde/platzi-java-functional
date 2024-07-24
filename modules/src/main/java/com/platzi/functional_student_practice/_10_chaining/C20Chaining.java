e7b21a3965c5ced6de16de7070f3434e17ff85e4
fiorde90 <fiorde_90@hotmail.com>
2024-07-24 18:25:50 -0300

    Clear of personal practice. Keep global config for java 21 and better gradle implementation

--- modules/src/main/java/com/platzi/functional_student_practice/_10_chaining/C20Chaining.java
+++ modules/src/main/java/com/platzi/functional_student_practice/_10_chaining/C20Chaining.java
@@ -2,36 +2,4 @@ package com.platzi.functional_student_practice._10_chaining;
 
 
 public class C20Chaining {
-    public static void main(String[] args) {
-        System.out.println("////////////////////////////////////////////////////////////////////////////////////");
-        System.out.println("\nCLASS 20\n");
-
-        // 1. Every .append() method returns "this" object with the changes updated in state.
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Hola ")
-                .append("alumno")
-                .append("!")
-                .reverse()
-                .append(" :iztalP")
-                .reverse();
-        System.out.println(stringBuilder);
-
-        // 2. Custom example. This is usefull with FUNCTION COMPOSITION
-        Chainer chainer = new Chainer();
-        Chainer chainer2 = chainer.sayHi();
-        Chainer chainer3 = chainer2.sayBye();
-        chainer3.sayHi().sayBye();
-    }
-
-    private static class Chainer {
-        public Chainer sayHi() {
-            System.out.println("Hola");
-            return this;
-        }
-
-        public Chainer sayBye() {
-            System.out.println("Bye");
-            return this;
-        }
-    }
 }
