5436cee73e62de041f543f79af58c76e4107a82d
fiorde90 <fiorde_90@hotmail.com>
2024-07-24 16:56:35 -0300

    class 30-40 JobSearchReporter

---
+++ modules/src/main/java/com/platzi/functional_student_practice/_20_job_search_reporter/C30A40JobSearchReporter.java
@@ -0,0 +1,10 @@
+package com.platzi.functional_student_practice._20_job_search_reporter;
+
+public class C30A40JobSearchReporter {
+    /*
+        al 24 de julio de 2024, la API https://jobs.github.com/api se encuentra fuera de servicio, por lo que no se
+        puede realizar practica real sobre los conceptos desarrollados en clase. Las clases y los contenidos se siguen
+        de manera teórica de aquí en adelante, usando como referencia el código del directorio root/jobs-search-reporter
+        y los conceptos desarrollados en videos y recursos del curso https://platzi.com/cursos/java-funcional/
+     */
+}
